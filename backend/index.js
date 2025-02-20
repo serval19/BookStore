e95c61530f6ab1f68a23833deb7a70c18949a4cb
Diwa Tomy <144988434+serval19@users.noreply.github.com>
2025-02-20 21:51:51 +0530

    new fils added

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import {PORT,mongoDBURL} from "./config.js";
+import mongoose from "mongoose";
+
+const app=express();
+app.get("/",(request,response)=>{
+    console.log(request)
+    return response.status(234).send('Welcome to MERN stack tutorial')
+})
+
+mongoose
+.connect(mongoDBURL)
+.then(()=>{
+    console.log("successfully connected to MongoDB");
+    app.listen(PORT,()=>{
+        console.log(`App is listening on  port: ${PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log(error);
+})
+
