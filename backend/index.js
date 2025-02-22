e761361f4fa40d7437c595edd08f39dbca2db479
Diwa Tomy <144988434+serval19@users.noreply.github.com>
2025-02-22 21:10:42 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -35,6 +35,32 @@ app.post("/books",async (request,response)=>{
         response.status(500).send({message: error.message})
     }
 })
+// route for getting all the books
+app.get("/books",async (request,response)=>{
+    try{
+        const books=await Book.find({})
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    }
+    catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+})
+// route for getting single book by id
+app.get("/books/:id",async (request,response)=>{
+    try{
+        const { id }=request.params;
+        const book=await Book.findById(id)
+        return response.status(200).json(book);
+    }
+    catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+})
 
 mongoose
 .connect(mongoDBURL)
