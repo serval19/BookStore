93cc5fe4c2f75c09256fedcd28ac87edde21c410
Diwa Tomy <144988434+serval19@users.noreply.github.com>
2025-02-27 20:34:43 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import CreateBooks from './pages/CreateBooks'
+import EditBooks from './pages/EditBooks'
+import ShowBook from './pages/ShowBook'
+import DeleteBook from './pages/DeleteBook'
 
 const App = () => {
   return (
-    <div className='bg-red-500 text-white'>App</div>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/books/create' element={<CreateBooks/>} />
+      <Route path='/books/details/:id' element={<ShowBook/>} />
+      <Route path='/books/edit/:id' element={<EditBooks/>} />
+      <Route path='/books/delete/:id' element={<DeleteBook/>} />
+    </Routes>
   )
 }
 
---
+++ frontend/src/pages/CreateBooks.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CreateBooks = () => {
+  return (
+    <div>CreateBooks</div>
+  )
+}
