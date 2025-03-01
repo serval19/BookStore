d9a3b59ab13c70dc47a8bb10866c0bf052c3c31e
Diwa Tomy <144988434+serval19@users.noreply.github.com>
2025-03-02 00:40:57 +0530

    n

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import spinner from '../components/Spinner'
+import Spinner from '../components/Spinner'
 import { Link } from 'react-router-dom'
 import { AiOutlineEdit } from 'react-icons/ai'
 import { BsInfoCircle } from 'react-icons/bs'
@@ -8,9 +8,36 @@ import { MdOutlineAddBox,MdOutlineDelete } from 'react-icons/md'
 export const Home = () => {
   const [books,setBooks]=useState([]);
   const [loading,setLoading]=useState(false);
+  useEffect(()=>{
+    setLoading(true);
+    axios.get('http://localhost:5555/books')
+    .then((response)=>{
+      setBooks(response.data.data);
+      setLoading(false);
+    })
+    .catch((error)=>{
+      console.log(error);
+      setLoading(false);
+    })
+  },[])
   return (
     
 
-    <div>Home</div>
+    <div className='p4'>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-3xl my-8'>Books List</h1>
+        <Link to='/books/create'>
+          <MdOutlineAddBox className='text-sky-800  text-4xl' />
+        </Link>
+      </div>
+      {loading ? (
+        <Spinner />
+      ):(
+        <table className='w-full border-seperate border-spacing-2'>
+          
+        </table>
+
+      )}
+    </div>
   )
 }
