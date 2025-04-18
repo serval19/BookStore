8142dc46004c6a3dc58ecf5cd917c1cf46363bfa
Diwa Tomy <144988434+serval19@users.noreply.github.com>
2025-04-19 00:43:34 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
   const [showType,setShowType]=useState('table')
   useEffect(() => {
     setLoading(true);
-    axios.get('http://localhost:5555/books')
+    axios.get('https://book-store-mernapp-api.vercel.app/books')
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
