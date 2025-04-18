caa8f3a8c30ca1757339c2c568a93e9469f85818
Diwa Tomy <144988434+serval19@users.noreply.github.com>
2025-04-19 00:42:15 +0530

    Update CreateBooks.jsx

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -19,7 +19,7 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-    .post('http://localhost:5555/books',data)
+    .post('https://book-store-mernapp-api.vercel.app/books',data)
     .then(()=>{
       setLoading(false);
       enqueueSnackbar('Book created successfully.',{variant:'success'})
