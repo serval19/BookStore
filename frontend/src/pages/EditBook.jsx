97cb80ba0b8f387372165ab8f165b7e48d71dc40
Diwa Tomy <144988434+serval19@users.noreply.github.com>
2025-04-19 00:42:49 +0530

    Update EditBook.jsx

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -14,7 +14,7 @@ const EditBook = () => {
   const {enqueueSnackbar}=useSnackbar();
   useEffect(()=>{
     setLoading(true);
-    axios.get(`http://localhost:5555/books/${id}`)
+    axios.get(`https://book-store-mernapp-api.vercel.app/books/${id}`)
     .then((response)=>{
       setAuthor(response.data.author)
       setPublishYear(response.data.publishYear)
